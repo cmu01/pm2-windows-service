a60363104d74b5a9f52e9caa2d4c5741731e440c
cmu <cmu@rocketsoftware.com>
2019-03-26 14:22:19 +0800

    use default evn

--- src/install.js
+++ src/install.js
@@ -19,14 +19,9 @@ module.exports = co.wrap(function*(name, no_setup) {
 
     yield common.admin_warning();
 
-    let setup_response = yield no_setup ? Promise.resolve({
+    let setup_response = yield Promise.resolve({
         perform_setup: false
-    }) : inquirer.prompt([{
-        type: 'confirm',
-        name: 'perform_setup',
-        message: 'Perform environment setup (recommended)?',
-        default: true
-    }]);
+    });
 
     if(setup_response.perform_setup) {
         yield setup();
